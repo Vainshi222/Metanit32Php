8d858701f1d140654113cf8976c644a54785f520
Vainshi222 <46302571+Vainshi222@users.noreply.github.com>
2019-01-09 00:27:54 +0300

    V1.0.1

--- Metanit32Php/Controllers/HomeController.cs
+++ Metanit32Php/Controllers/HomeController.cs
@@ -20,6 +20,21 @@ namespace Metanit32Php.Controllers
         {
             return View(db.Phones.ToList());
         }
-        
+
+        [HttpGet]
+        public IActionResult Buy(int id)
+        {
+            ViewBag.PhoneId = id;
+            return View();
+        }
+        [HttpPost]
+        public string Buy(Order order)
+        {
+            db.Orders.Add(order);
+            // сохраняем в бд все изменения
+            db.SaveChanges();
+            return "Спасибо, " + order.User + ", за покупку!";
+        }
+
     }
 }
